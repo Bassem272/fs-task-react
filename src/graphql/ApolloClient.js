35d23afc6c52d0f97bd88a654ee8aec3ca6d25d3
bassem <bassem.gehad95@yahoo.com>
2025-01-19 10:20:09 +0200

    change uri for ngrok

--- src/graphql/ApolloClient.js
+++ src/graphql/ApolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8090', // Replace with your GraphQL endpoint
+  uri: 'https://adbc-197-32-83-84.ngrok-free.app', 
   cache: new InMemoryCache(),
 });
 
@@ -10,4 +10,4 @@ const ApolloClientProvider = ({ children }) => (
 );
 
 export { ApolloClientProvider };
-// uri: 'https://f22c-197-32-64-10.ngrok-free.app', // Replace with your GraphQL endpoint
+// uri: 'http://localhost:8090', 
